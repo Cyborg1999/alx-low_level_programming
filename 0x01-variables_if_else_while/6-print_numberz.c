8b761088892cd1030229fbb35ac2e0a23898910f
Cyborg1999 <andrewk.tonui@gmail.com>
2022-03-11 14:07:32 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print numbers from 0 to 9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int y;
+	for (y = 48; y < 58; y++)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
+}
