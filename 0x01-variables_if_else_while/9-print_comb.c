8e13093361535a3bfdf7d8ba334a55b790a610c5
Cyborg1999 <andrewk.tonui@gmail.com>
2022-03-11 14:33:45 -0800

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * Description: print numbers 0 to 9  with ","
+ * Return: Always 0 
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = 0; y < 10; y++)
+	{
+		putchar(" %i,", y);
+	}
+	putchar('\n');
+	return (0);
+}
