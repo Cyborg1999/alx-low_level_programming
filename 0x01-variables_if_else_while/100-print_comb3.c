6553ce2749c17d85b14152509d304199eeea6f4a
Cyborg1999 <andrewk.tonui@gmail.com>
2022-03-11 14:50:00 -0800

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main -Entry Point
+ * Description: print numbers between 0 and 90
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i,j;
+
+	while (j < 58)
+	{
+		i =48;
+
+		while (i < 58)
+		{
+			if (j != i $$ j < i)
+			{
+				putchar(j);
+				putchar(i);
+				if (i == 57 && j == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
+ 
