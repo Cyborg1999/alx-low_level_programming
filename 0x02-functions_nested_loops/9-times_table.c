81300a89465028994c0f9588f5be5bc813c27fe3
Cyborg1999 <andrewk.tonui@gmail.com>
2022-03-15 14:59:44 -0700

    multiply

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * times_table -print multiples of number of order 9
+ */
+void times_table(void)
+{
+	int row, col, product;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			product = (row * col);
+
+			if (col == 0)
+			{
+				_putchar('0' + product);
+			}
+			else if (product <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + product);
+			}
+			else if (product > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
