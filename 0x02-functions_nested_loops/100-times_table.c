5b26947b99d07d09b81df51b7473ee507ed1f291
Cyborg1999 <andrewk.tonui@gmail.com>
2022-03-15 15:47:12 -0700

    century

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,54 @@
+i#include "main.h"
+/**
+ * print_times_table -print the n times table
+ *
+ * @n: number of times (0 < n <= 15)
+ *
+ * Return: no return
+ */
+void print_times_table(int n)
+{
+	int row, col, sum;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (col = 0; col <= n; col++)
+			{
+				sum = (row * col);
+
+				if (col == 0)
+				{
+					_putchar('0' + sum);
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+
+					if (sum <= 9)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + sum);
+					}
+					else if (sum > 9 && sum < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + (sum / 10));
+						_putchar('0' + (sum % 10));
+
+					}
+					else if ( sum >= 100)
+					{
+						_putchar('0' + (sum / 100));
+						_putchar('0' + ((sum % 100) % 10);
+						_putchar('0' + (sum % 10));
+					}
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
