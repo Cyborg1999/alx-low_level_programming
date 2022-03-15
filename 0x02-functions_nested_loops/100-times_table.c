d856dea4cd50483298ebf1243d9323aad887d695
Cyborg1999 <andrewk.tonui@gmail.com>
2022-03-15 15:49:33 -0700

    brackets

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -43,7 +43,7 @@ void print_times_table(int n)
 					else if ( sum >= 100)
 					{
 						_putchar('0' + (sum / 100));
-						_putchar('0' + ((sum % 100) % 10);
+						_putchar('0' + ((sum % 100) % 10));
 						_putchar('0' + (sum % 10));
 					}
 				}
