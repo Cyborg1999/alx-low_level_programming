eda2d47cc90b41f17684581bffdbde2100d112de
Cyborg1999 <andrewk.tonui@gmail.com>
2022-03-15 10:49:43 -0700

    lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - check the code
+ * print_alphabet - Prints the lowercase alphabet letters
  *
  * Return: Always o.
  */
@@ -8,7 +8,7 @@ void print_alphabet(void)
 {
 	int i;
 
-	for (i = 97; i < 122; i++)
+	for (i = 97; i < 123; i++)
 	{
 		_putchar(i);
 	}
