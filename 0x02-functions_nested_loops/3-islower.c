8e2ad5b5c5d24a460df9c85be4b2556396aacd6f
Cyborg1999 <andrewk.tonui@gmail.com>
2022-03-15 11:37:05 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * islower - function to find if a character is lowercase
+ *
+ * Return: Always 0 (Success).
+ */
+void islower(void)
+{
+	int i;
+
+	if (int i > 96 && i < 123)
+	{
+		_putchar(1);
+	}
+	else
+	{
+		_putchar(0);
+	}
+}
+
