71e29b36c693e7c9836c62d75ac0b15c4285712c
Cyborg1999 <andrewk.tonui@gmail.com>
2022-03-17 14:16:11 -0700

    buzzing

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,35 +6,34 @@
  */
 int main(void)
 {
-	int n = 1;
+	int i = 1;
 
-	while (n <= 100)
+	while (i <= 100)
 	{
-	if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else
-		{
-			printf("%i", i);
-		}
+		if (i % 3 == 0 && i % 5 == 0)
+				{
+				printf("FizzBuzz");
+			}
+			else if (i % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			else if (i % 5 == 0)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("%i", i);
+			}
 
-		if (i != 100)
-		{
-			putchar(' ');
-		}
+			if (i != 100)
+			{
+				putchar(' ');
+			}
 
-		i++;
+			i++;
 	}
 	putchar('\n');
 	return (0);
-	
 }
