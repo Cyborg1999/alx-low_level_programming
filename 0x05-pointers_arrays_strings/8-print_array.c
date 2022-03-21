cbec981a0c777f2431ebf8e3516b2d972f6c398c
Cyborg1999 <andrewk.tonui@gmail.com>
2022-03-21 15:19:09 -0700

    present

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,9 +11,9 @@ void print_Array(int *a, int n)
 {
 	int i = 0;
 
-	for (n--;  n >= 0;n--, i++)
+	for (n--;  n >= 0; n--, i++)
 	{
-		printf("%d", a [i]);
+		printf("%d", a[i]);
 
 		if (n > 1)
 			printf(", ");
