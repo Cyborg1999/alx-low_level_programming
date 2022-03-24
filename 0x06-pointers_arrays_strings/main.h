c088c1a0ffc2df962681308a7be50c12e5390b60
Cyborg1999 <andrewk.tonui@gmail.com>
2022-03-24 04:26:25 -0700

    reverse strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strvcpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+void reverse-array(int *a, int n);
 
 
 #endif
