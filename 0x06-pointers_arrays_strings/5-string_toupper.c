17ce59cbe0f79c6e9d9b11c6a5b3d45b3cd33441
Cyborg1999 <andrewk.tonui@gmail.com>
2022-03-24 06:24:18 -0700

    5 cap

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,7 @@
 /**
  * string_toupper - changes all lowercase letters of a string
  * to uppercase
- * @s: input string
+ * @str: pointer
  * Return: the pointer to dest
  */
 char *string_toupper(char *str)
