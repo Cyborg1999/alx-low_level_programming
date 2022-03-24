21478af74e835fb808209ca7b7667263fa232eda
Cyborg1999 <andrewk.tonui@gmail.com>
2022-03-24 03:19:49 -0700

    return

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,7 @@
  * _strcat - concatenates two strings,
  * @dest: destinations.
  * @src: source.
- * @Return: the pointer to dest.
+ * Return: the pointer to dest.
  */
 char **_strcat(char *dest, char *src)
 {
