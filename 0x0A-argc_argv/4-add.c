a216b70254fdf45988204dd98ff9037885c0d948
Cyborg1999 <andrewk.tonui@gmail.com>
2022-04-01 13:38:34 -0700

    addition

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * isInteger - checks if s is an integer
+ * @s: string to check 
+ * Return: 0 or 1
+ */
+int isInteger(const char *s)
+{
+	int i = 0;
+	while(--argc)
+	{
+		if (isInteger(argv[argc]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[argc]);
+	}
+
+	printf("%i\n", sum);
+
+	return (0);
+}
