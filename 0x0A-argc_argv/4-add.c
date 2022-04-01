642883b13f6173cb219818f862d61e022bc45661
Andrew Rotich <AndrewK.Tonui@gmail.com>
2022-04-01 23:40:38 +0300

    Update addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,18 +7,37 @@
  */
 int isInteger(const char *s)
 {
-	int i = 0;
-	while(--argc)
+int i = 0;
+while (s[i] != '\0')
+{
+	if (s[i] < '0' || s[i] > '9')
+		return (1);
+	i++;
+}
+return (0);
+}
+
+/**
+ * main - adds positive numbers
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+int sum = 0;
+while (--argc)
+{
+	if (isInteger(argv[argc]))
 	{
-		if (isInteger(argv[argc]))
-		{
-			printf("Error\n");
-			return (1);
-		}
-		sum += atoi(argv[argc]);
+		printf("Error\n");
+		return (1);
 	}
+	sum += atoi(argv[argc]);
+}
 
-	printf("%i\n", sum);
+printf("%i\n", sum);
 
-	return (0);
+return (0);
 }
