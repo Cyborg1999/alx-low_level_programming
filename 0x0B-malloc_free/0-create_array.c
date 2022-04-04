7d90a1307b5bd8e320c2b6fa59d15cb0dde2a1a3
Cyborg1999 <andrewk.tonui@gmail.com>
2022-04-04 03:56:42 -0700

    size

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,12 +13,15 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (null);
-	p = malloc((size) * sizeof(char));
-	if (*p == '\0')
+
+	p = malloc(sizeof(char) * size);
+
+	if (p == '\0')
 		return (null);
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
 	}
+
 	return (p);
 }
