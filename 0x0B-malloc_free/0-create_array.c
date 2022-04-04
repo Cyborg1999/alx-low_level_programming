f46e7aeccdf40b24fac337a0577f4fda89e5a45e
Cyborg1999 <andrewk.tonui@gmail.com>
2022-04-04 04:08:06 -0700

    100 changes

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,13 +14,13 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (0);
 
-	p = malloc((size) * sizeof(char) + 1);
+	p = malloc((size) * sizeof(char));
 
 	if (*p == '\0')
 		return (0);
 	for (i = 0; i < size; i++)
 	{
-		p[i] = c;
+		*p[i] = c;
 	}
 
 	return (p);
