48d21a5450fbe810f50ef4a7738f9bc58f300f8e
Cyborg1999 <andrewk.tonui@gmail.com>
2022-04-08 01:02:52 +0300

    rello

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,26 +9,43 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *clone, *relloc;
-	unsigned int i;
+	void *mem;
+	char *ptr_copy, *filler;
+	unsigned int index;
 
-	if (ptr != NULL)
-		clone = ptr;
-	else
-	{
-		return (malloc(new_size));
-	}
 	if (new_size == old_size)
 		return (ptr);
+
+	if (ptr == NULL)
+	{
+		mem = malloc(new_size);
+
+		if (mem == NULL)
+			return (NULL);
+
+		return (mem);
+	}
+
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return (0);
+		return (NULL);
 	}
-	for (i = 0; i < (old_size || i < new_size); i++)
+
+	ptr_copy = ptr;
+	mem = malloc(sizeof(*ptr_copy) * new_size);
+
+	if (mem == NULL)
 	{
-		*(relloc + i) = clone[i];
+		free(ptr);
+		return (NULL);
 	}
+
+	filler = mem;
+
+	for (index = 0; index < old_size && index < new_size; index++)
+		filler[index] = *ptr_copy++;
+
 	free(ptr);
-	return (relloc);
+	return (mem);
 }
