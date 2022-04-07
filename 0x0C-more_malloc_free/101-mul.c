77a4b9ba8e9764a3becd844f7abb609a7d28d4a1
Cyborg1999 <andrewk.tonui@gmail.com>
2022-04-07 15:09:08 -0700

    incomplete

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main -multiplies two positive numbers
+ * @argc: n  arguements
+ * @argv: args
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	unsigned long mul;
+	int i, j;
+
+	if (argc != 3)
+	{
+
+	}
+}
