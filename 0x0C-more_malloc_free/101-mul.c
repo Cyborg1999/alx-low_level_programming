23e6b7bb627c792b2b1876208f6b3ea3c536aad7
Cyborg1999 <andrewk.tonui@gmail.com>
2022-04-09 02:28:23 +0300

    tf2

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,16 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-<<<<<<< HEAD
-=======
-#include <stdio.h>
-
-
-int find_len(char *str);
-char *create_xarray(int size);
-char *iterate_zeroes(char *str);
-void get_prod(char *prod, char *mult, int digit, int zeroes);
-void add_nums(char *final_prod, char *next_prod, int next_len);
->>>>>>> f41432a7437eb7565d558c5773f61e1c827eee2f
 
 /**
  * _print - moves a string one place to the left and prints the string
