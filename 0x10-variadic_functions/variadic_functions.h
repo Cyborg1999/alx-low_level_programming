b32e597d65ee72121d533b2ddf8fd9473a6508c3
Cyborg1999 <andrewk.tonui@gmail.com>
2022-04-14 06:15:21 -0700

    file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,7 +7,6 @@
 int sum_them_all(const unsigned int n, ...);
 
 void print_numbers(const char *separator, const unsigned int n, ...);
-#include <stdio.h>
 void print_strings(const char *separator, const unsigned int n, ....);
 void print_all(const char * const format, ...);
 
