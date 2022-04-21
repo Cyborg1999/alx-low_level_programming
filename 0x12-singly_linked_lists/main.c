8289e9bd676df66d49fa44803354d210816d59e8
Cyborg1999 <andrewk.tonui@gmail.com>
2022-04-21 11:39:58 -0700

    merge

--- 0x12-singly_linked_lists/main.c
+++ 0x12-singly_linked_lists/main.c
@@ -9,8 +9,7 @@
  * Return: Always 0.
  */
 int main(void)
-{
-    list_t *head;
+{ list_t *head;
     list_t *new;
     list_t hello = {"World", 5, NULL};
     size_t n;
@@ -20,6 +19,7 @@ int main(void)
     if (new == NULL)
     {
         printf("Error\n");
+
         return (1);
     }
     new->str = strdup("Hello");
