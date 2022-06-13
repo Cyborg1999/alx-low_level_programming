0f8291d84fdd42b2c3ce1f5596e2b06dd7b3b473
Cyborg1999 <andrewk.tonui@gmail.com>
2022-06-14 00:24:06 +0300

    win 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget /tmp/https://github.com/Cyborg1999/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
-export LD_PRELOAD= /tmp/win.so
+wget ../tmp/https://github.com/Cyborg1999/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD= ../tmp/win.so
