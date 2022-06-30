c9c43625fcd99feaf9c0fde16083a1e5d7d84cc0
Cyborg1999 <andrewk.tonui@gmail.com>
2022-06-30 15:51:55 +0300

    [Task]: define header tests

---
+++ 0x1A-hash_tables/tests/0-main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "../hash_tables.h"
+
+/**
+ * main -check the code
+ *
+ * Return: Always XIT_SUCCESS.
+ */
+int main(void)
+{
+    hash_node_t *ht;
+
+    ht =hash_table_create(1024);
+    printf("%p\n", (void *)ht);
+    return (EXIT_SUCCESS);
+}
